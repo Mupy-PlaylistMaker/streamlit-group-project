44f20525c9e4fffbac5a91b43b1c49b4e9b27642
Leo3499 <leonardo.gonnelli@student.unisg.ch>
2025-03-27 19:05:33 +0100

    Update New Version(Leo).py

--- New Version(Leo).py
+++ New Version(Leo).py
@@ -1,6 +1,21 @@
-#importing needed packages
+# Importing needed packages
 import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+
+# Spotify credentials
+CLIENT_ID = "61465f148fcb406b856636263e273525"
+CLIENT_SECRET = "d96d3207f4da4bc1a7f302685f6f8099"
+REDIRECT_URI = " "
+SCOPE = "user-read-private user-read-email playlist-modify-private user-library-read user-top-read" # List of interactions with Spotify account authorized for Mupy
+
+# OAuth setup (how the app will have access to the spotify account)
+sp_oauth = SpotifyOAuth(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri=REDIRECT_URI,
+    scope=SCOPE
+)
+
 
 
